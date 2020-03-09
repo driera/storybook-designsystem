d0e340bac22cf85b2dc1f4eca4df1bda58f2a835
Dani Riera <daniel.riera@qatium.com>
2020-03-09 10:37:29 +0100

    Update chromatic config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import { GlobalStyle } from '../src/shared/global';
+import 'storybook-chromatic';
 
 addDecorator(story => (
   <>
